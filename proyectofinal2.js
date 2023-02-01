7c0f8e98713f70f3b33e405dc1043300dd843142
molokonadsat <molokonadsat@gmail.com>
2023-01-31 22:13:09 -0300

    se agrega el proyecto

---
+++ proyectofinal2.js
@@ -0,0 +1,4 @@
+document.getElementById("botonreferencias").onclick=function(){
+console.log("aparecen las referencias");
+document.getElementById("textoreferencias").innerHTML="JUAN PEREZ ENCARGADO INDUMENTARIA FEMENINA 'ABC' 011 13446342 ";    
+}
